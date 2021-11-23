a16c0449cec00537e96b4dd70cdc60c2e9bf1fc7
Dmitriy Smotrov <Smotrov1991@yandex.ru>
2021-11-24 00:00:01 +0300

    review and last part

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,32 @@
 $(function() {
-  $('.slider__wrapper').slick({
-    prevArrow: $('.prev'),
-    nextArrow: $('.next'),
+
+  $('.slider-brands__wrapper').slick({
+    arrows: false,
+    slidesToShow: 6
+  });
+
+  $('.slider-promotion__wrapper').slick({
+    prevArrow: $('.slider-promotion__prev'),
+    nextArrow: $('.slider-promotion__next'),
     infinite: false,
     fade: true
   });
 
-  var mixer = mixitup('.product__catalog', {
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    },
     classNames: {
-      block: 'menu',
-      elementFilter: 'link',
+      block: 'filter-nav',
+      elementFilter: 'btn',
       delineatorElement: '__',
       delineatorModifier: '--'
     }
-});
+  };
+ 
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 });
